73eea3b49d62a0bdd85e78400b095fbac632a401
Edgar Kurm <edgar.kurm@gmail.com~>
2022-05-12 14:07:47 +0300

    week15 change

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const { default: axios } = require('axios');
 const express = require('express');
-const http = require ('http');
+const http = require('http');
+const https = require('https');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 app.get('/', (req, res) => {
 
     
-    let url = 'https://api.themoviedb.org/3/movie/550988?api_key=93e2273331cd7f30d1433d9ae448e713';
+    let url = 'https://api.themoviedb.org/3/movie/550988?api_key=4ca94f8b470d7e34bd3f59c3914295c8';
     axios.get(url)
     .then(response => {
         console.log(response.data.title);
@@ -32,61 +32,9 @@ app.get('/', (req, res) => {
         res.render('index', {movieData: data, releaseDate: releaseDate, genres: genresUpdated, poster: moviePoster, year: currentYear});
     });
     
-})
-
-app.get('/search', (req, res) => {
-    res.render('search', {movieData:''});
 });
 
-
-app.post('/search', (req, res) => {
-    let movieTitle = req.body.movieTitle;
-
-    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=4ca94f8b470d7e34bd3f59c3914295c8&query=${movieTitle}`;
-    let genres = 'https://api.themoviedb.org/3/genre/movie/list?api_key=4ca94f8b470d7e34bd3f59c3914295c8&language=en-US';
-   
-    let endpoints = [movieUrl, genres];
-
-    axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
-    .then(
-        axios.spread((movie, genres) => {
-            const [general, generalComingSoon] = movie.data.results;
-            const movieGenreIds = general.genre_ids;        
-            const movieGenres = genres.data.genres; 
-            
-            let genresArray = [];
-            for(let i = 0; i < movieGenreIds.length; i++) {
-                for(let j = 0; j < movieGenres.length; j++) {
-                    if(movieGenreIds[i] === movieGenres[j].id) {
-                        console.log(movieGenres[j].name);
-                        genresArray.push(movieGenres[j].name)
-                    }
-                    
-                }
-            }
-
-            let genresToDisplay = '';
-            genresArray.forEach(genre => {
-                genresToDisplay = genresToDisplay+ `${genre}, `;
-            });
-
-            genresToDisplay = genresToDisplay.slice(0, -2) + '.';
-            
-            let movieObject = {
-                title: general.original_title,
-                year: new Date(general.release_date).getFullYear(),
-                overview: general.overview,
-                posterUrl: `https://image.tmdb.org/t/p/w500/${general.poster_path}`,
-                genres: genresToDisplay
-
-            };
-
-            res.render('search', {movieData: movieObject});
-        })
-      );
-    
-});
-
+//new route
 app.post('/getmovie', (req, res) => {
 	const movieToSearch =
 		req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie
@@ -94,7 +42,7 @@ app.post('/getmovie', (req, res) => {
 			: '';
 
 	const reqUrl = encodeURI(
-		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=d7693819`
+		`http://www.omdbapi.com/?t=${movieToSearch}&apikey=cc6f9d1b`
 	);
 	http.get(
 		reqUrl,
@@ -131,6 +79,7 @@ app.post('/getmovie', (req, res) => {
 		}
 	)
 });
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('server is running');
