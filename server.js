7500aa5715cedec01e8b14ff8922c9826b3345da
Edgar Kurm <edgar.kurm@gmail.com~>
2022-05-13 13:54:15 +0300

    repairing

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const { default: axios } = require('axios');
 const express = require('express');
-
 const app = express();
-
+ 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.json());
-
-
+app.use(express.urlencoded({ extended: true }));
+ 
+ 
 app.get('/', (req, res) => {
-
-    
     let url = 'https://api.themoviedb.org/3/movie/550988?api_key=4ca94f8b470d7e34bd3f59c3914295c8';
     axios.get(url)
     .then(response => {
@@ -18,21 +15,21 @@ app.get('/', (req, res) => {
         let data = response.data;
         let releaseDate = new Date(data.release_date).getFullYear();
         let genres = '';
-
+ 
         data.genres.forEach(genre => {
             genres = genres + `${genre.name}, `;
         });
-
-        
+ 
+ 
         let genresUpdated = genres.slice(0, -2) + '.';
         moviePoster = `https://image.tmdb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
         console.log(genresUpdated);
         let currentYear = new Date().getFullYear();
         res.render('index', {movieData: data, releaseDate: releaseDate, genres: genresUpdated, poster: moviePoster, year: currentYear});
     });
-    
+ 
 });
-
+ 
 app.get('/search', (req, res) => {
     res.render('search', { movieDetails:'' });
 });
@@ -80,8 +77,10 @@ app.post('/search', (req, res) => {
     }));
  
 });
-
-
+ 
+app.listen(process.env.PORT || 3000, () => {
+    console.log('server is running');
+});
 
 
 
